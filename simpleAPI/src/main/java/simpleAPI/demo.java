20bd0d45d8ef5adf5a5547b78360b51249214d1c
Your Name <you@example.com>
2022-03-04 19:01:45 +0530

    commited

---
+++ simpleAPI/src/main/java/simpleAPI/demo.java
@@ -0,0 +1,30 @@
+package simpleAPI;
+      import java .util.Arrays;
+		 import java.util.List;
+		 public class demo{
+
+		 	public static void main(String args[]) {
+		 	
+		 	{
+		 		List<Integer>Values=Arrays.asList(1,2,3,4,5);
+		 		
+		 		 for(int i:Values)
+
+		 	{
+		 			 System.out.println(i);
+		 	}
+		 		class InnerClass	{
+		 			int y =6;
+		 			
+		 		}
+		 		InnerClass ic= new  InnerClass();
+		 	
+		 		
+		 		System.out.println(ic);
+		 		
+		 	}
+		 }
+		 
+	}
+
+
